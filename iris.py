0273234f26624d1f8638ce1225cfea065a8596f2
gus <gjuantorena@gmail.com>
2024-04-17 12:50:31 -0300

    iris

---
+++ iris.py
@@ -0,0 +1,35 @@
+from seaborn import load_dataset
+from julearn import PipelineCreator, run_cross_validation
+from julearn.utils import configure_logging
+from sklearn.model_selection import StratifiedKFold
+from julearn.viz import plot_scores
+
+
+def main():
+    configure_logging(level="INFO")
+
+    df_iris = load_dataset("iris")
+
+    df_iris = df_iris[df_iris["species"].isin(["versicolor", "virginica"])]
+
+    X = ["sepal_length", "sepal_width", "petal_length"]
+
+    y = "species"
+
+    cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+
+    creator = PipelineCreator(problem_type="classification")
+    creator.add("zscore")
+    creator.add("svm")
+
+    scores = run_cross_validation(
+        X=X, y=y, data=df_iris, cv=cv, model=creator, return_train_score=True
+    )
+
+    print(scores)
+    panel = plot_scores(scores)
+    panel.show()
+
+
+if __name__ == "__main__":
+    main()
